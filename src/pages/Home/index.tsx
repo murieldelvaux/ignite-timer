46870f7173c00d4f10597bfd25e9df254e4c5482
Muriel Delvaux Daher <muriel.delvaux@geb.inatel.br>
2023-01-02 11:45:16 -0300

    feat: use react hook form with zod lib for validations

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,60 @@
 import { Play } from "phosphor-react";
-import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from "./styles";
+import { 
+    CountDownContainer, 
+    FormContainer, 
+    HomeContainer, 
+    MinutesAmountInput, 
+    Separator, 
+    StartCountDownButton, 
+    TaskInput 
+} from "./styles";
+
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import * as zod from 'zod'
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "Informe a tarefa"),
+    minutesAmount: zod.number().min(5, "O valor deve ser no mínimo de 5 minutos").max(60, "O valor deve ser menor do que 60 minutos"),
+})
+
+/* interface NewCycleFormData{
+    task: string;
+    minutesAmount: number;
+}
+*/
+
+/* dessa forma não precisamos criar uma interface, pois ele cria automaticamente com o zod */
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home(){
+    const {register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        /* Como boa prática, coloque sempre os defaultValues para a função reset funcionar corretamente */
+        defaultValues:{
+            task:'',
+            minutesAmount:0,
+        }
+    });
+
+    const task = watch('task');
+    const isSubmitDisabled = !task;
+    
+    function handleCreateNewCycle(data: NewCycleFormData){
+        console.log(data);
+        reset();
+    }
+
     return(
         <HomeContainer>
-            <form action="">
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput 
-                        type="task" 
-                        placeholder="Dê um nome para o seu projeto"
+                        id="task" 
                         list="task-suggestions"
+                        placeholder="Dê um nome para o seu projeto"
+                        {...register('task')}
                     />
                     <datalist id="task-suggestions">
                         <option value={"projeto 1"} />
@@ -26,6 +70,7 @@ export function Home(){
                         step={5}
                         min={5}
                         max={60}
+                        {...register('minutesAmount', {valueAsNumber: true})}
                     />
 
                     <span>minutos.</span>
@@ -40,7 +85,7 @@ export function Home(){
                     <span>0</span>
                 </CountDownContainer>
 
-                <StartCountDownButton disabled type="submit">
+                <StartCountDownButton disabled={isSubmitDisabled} type="submit">
                     <Play size={24} />
                     Começar
                 </StartCountDownButton>
